f100651ff5310dc495a5b738fc5b0f95824d76c3
Julius Venier <julius.venier@gmail.com>
2024-06-13 16:17:08 +0200

    Init

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron')
+const fs = require("node:fs");
+const os = require("node:os");
+const replace = require('replace-in-file');
+
+const CONFIG_FILE_PATH = os.homedir() + "/angarium.json";
+
+async function loadConfig() {
+    let config = JSON.parse(fs.readFileSync(CONFIG_FILE_PATH).toString())
+
+    const options = {
+        files: "./index.html",
+        from: [/{SCHEMA}/g, /{HOST}/g, /{PORT}/g],
+        to: [config.scheme, config.host, config.port]
+    }
+    await replace(options)
+}
+
+const createWindow = async () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    try {
+        await loadConfig();
+    } catch (error){
+        console.error('An Error occurred: ', error)
+        win.loadFile('src/error.html')
+        return;
+    }
+
+    win.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
